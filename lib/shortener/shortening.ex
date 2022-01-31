91959196d7cb1845af74cd92621661bbb010e707
Gonzalo <grzuy0@gmail.com>
2022-01-31 16:47:22 -0300

    feat: improve validation of Link format

--- lib/shortener/shortening.ex
+++ lib/shortener/shortening.ex
@@ -2,6 +2,10 @@ defmodule Shortener.Shortening do
   use Ecto.Schema
   import Ecto.Changeset
 
+  # Very naive and simplified format
+  # Could use improevement
+  @url_format_regex ~r/\Ahttps?:\/\/.+\..+\z/
+
   schema "shortenings" do
     field :slug, :string
     field :target_url, :string
@@ -14,5 +18,6 @@ defmodule Shortener.Shortening do
     shortening
     |> cast(attrs, [:slug, :target_url])
     |> validate_required([:slug, :target_url])
+    |> validate_format(:target_url, @url_format_regex)
   end
 end
--- test/shortener_web/controllers/shortening_controller_test.exs
+++ test/shortener_web/controllers/shortening_controller_test.exs
@@ -4,7 +4,7 @@ defmodule ShortenerWeb.ShorteningControllerTest do
   alias Shortener.{Repo, Shortening}
 
   describe "create" do
-    test "invalid url", %{conn: conn} do
+    test "missing url", %{conn: conn} do
       original_count = Repo.aggregate(Shortening, :count)
 
       conn =
@@ -19,6 +19,40 @@ defmodule ShortenerWeb.ShorteningControllerTest do
 
       # No new shortening created
       assert Repo.aggregate(Shortening, :count) == original_count
+    end
+
+    test "invalid url", %{conn: conn} do
+      original_count = Repo.aggregate(Shortening, :count)
+
+      conn =
+        post(
+          conn,
+          Routes.shortening_path(conn, :create),
+          shortening: %{url: "https://incomplete"}
+        )
+
+      assert get_flash(conn, :error) == "Link is invalid"
+      assert redirected_to(conn) == "/"
+
+      # No new shortening created
+      assert Repo.aggregate(Shortening, :count) == original_count
+    end
+
+    test "valid url", %{conn: conn} do
+      original_count = Repo.aggregate(Shortening, :count)
+
+      conn =
+        post(
+          conn,
+          Routes.shortening_path(conn, :create),
+          shortening: %{url: "https://valid.com"}
+        )
+
+      assert get_flash(conn, :error) == nil
+      assert redirected_to(conn) == "/"
+
+      # No new shortening created
+      assert Repo.aggregate(Shortening, :count) == original_count + 1
     end
   end
 end
