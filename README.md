2955aa0931aa9e0c02d4febc943f3f8cd29bc980
Gonzalo <grzuy0@gmail.com>
2022-02-01 10:34:18 -0300

    docs: improve documentation and notes

--- README.md
+++ README.md
@@ -1,18 +1,17 @@
-# Shortener
+# A Link Shortener
+
+Not production ready. Just for fun.
 
 [![Conventional Commits](https://img.shields.io/badge/Conventional%20Commits-1.0.0-yellow.svg)](https://conventionalcommits.org)
 ![CI workflow](https://github.com/grzuy/als/actions/workflows/ci.yml/badge.svg)
 
-To start your Phoenix server:
-
-  * Install dependencies with `mix deps.get`
-  * Create and migrate your database with `mix ecto.setup`
-  * Start Phoenix endpoint with `mix phx.server` or inside IEx with `iex -S mix phx.server`
+  * Install dependencies with `make setup`
+  * Start web app with `make server`
 
 Now you can visit [`localhost:4000`](http://localhost:4000) from your browser.
 
 ### Running tests
 
-Run tests with `$ mix test`.
+Run tests with `make test`.
 
-Run `$ SHOW_BROWSER=true mix test` to run tests with a visible browser window.
+Run `SHOW_BROWSER=true make test` to run tests with a visible browser window.
--- lib/shortener/slugs.ex
+++ lib/shortener/slugs.ex
@@ -1,9 +1,28 @@
+# TODO: Better algorithm to generate slugs.
+#
+# General properties to consider about the slug generation algorithm:
+#   - Collision avoidance
+#   - URL enumeration resistance
+#   - Concurrency
+#   - Consistency
+#   - Change in the performance of the algorithm as free slug count
+#     decreases with time
+#   - Accidental profanity in slugs
+#
+# Possible options:
+#
+#  A. Base62 encoding of shortenings.id for collision-avoidance plus a
+#     two digit checksum to avoid enumeration shortened URLs
+#  B. Have a process (or many with load balancing) be responsible for
+#     producing unique/random/unused slugs to avoid collisions. Could
+#     have a large number preemptively produced stored in a cache ready
+#     to be consumed to improve performance.
 defmodule Shortener.Slugs do
   @slug_length 6
   @slug_alphabet Enum.concat([?0..?9, ?A..?Z, ?a..?z])
 
   def new_slug() do
-    # Naive implementation. Possible collisions.
+    # Simple but naive implementation. Possible collisions.
     Enum.map(
       1..@slug_length,
       fn _ -> Enum.random(@slug_alphabet) end
